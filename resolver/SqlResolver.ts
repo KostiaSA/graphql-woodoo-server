1f09d27329e2040b4d00f2b5b5677a49a046cb4b
KostiaSA <9675795@gmail.com>
2020-02-22 21:49:41 +0300

    .

--- resolver/SqlResolver.ts
+++ resolver/SqlResolver.ts
@@ -14,20 +14,6 @@ export class SqlResolver {
     sql: SqlSelectBuilder = new SqlSelectBuilder();
     rowMapper: string[] = [];
 
-
-    rowMapper2: any = new Function(`
-    return function (row) { return {
-      num: row.num_0,
-      name: row.name_1,
-      vid_id: row.vid_id_2,
-      vid: {
-         id: row.vid_id_3,
-         num: row.vid_num_4,
-         name: row.vid_name_5,
-      }
-    }}    
-    `)();
-
     constructor(_schema: Schema, _args: Args, _info: GraphQLResolveInfo) {
         this.schema = _schema;
         this.args = _args;
@@ -72,20 +58,6 @@ export class SqlResolver {
 
     }
 
-    // rowMapper(r: any) {
-    //     return {
-    //         num: r.num_0,
-    //         name: r.name_1,
-    //         vid_id: r.vid_id_2,
-    //         vid: {
-    //             id: r.vid_id_3,
-    //             num: r.vid_num_4,
-    //             name: r.vid_name_5,
-    //         }
-    //     }
-
-    // }
-
     processFieldNode(f: SelectionNode, database: IDatabase, table: ITable, col: IColumn, fromAlias: string, levelFieldName: string) {
 
         if (f.kind == "Field") {
@@ -97,12 +69,13 @@ export class SqlResolver {
                 let join_table = this.schema.getTable(col.ref_db, col.ref_table);
                 let join_table_alias = this.schema.getTableObjectAlias(join_table);
                 let join_database = this.schema.getTableDatabase(join_table);
-                let join_fromAlias = fromAlias + "_" + join_table_alias;
+                let join_fromAlias = fromAlias + "_" + join_table_alias + "_" + this.sql.from.sql.length;
 
                 this.rowMapper.push(`${f.name.value}:{`);
 
                 let joinStr = "LEFT JOIN " + this.schema.getTable3PartName(join_table) + " AS " + join_fromAlias;
-                let joinOnColumnsList: string[] = col.ref_columns.map((item) => fromAlias + ".[" + item.column + "] = " + join_fromAlias + ".[" + item.ref_column + "]");
+                //identifierAsSql
+                let joinOnColumnsList: string[] = col.ref_columns.map((item) => fromAlias + "." + this.schema.identifierAsSql(database.type, item.column) + " = " + join_fromAlias + "." + this.schema.identifierAsSql(database.type, item.ref_column));
                 let joinOnStr = "ON " + joinOnColumnsList.join(" AND ");
                 this.sql.from.add(joinStr);
                 for (let ff of f.selectionSet.selections) {
@@ -173,7 +146,7 @@ export class SqlResolver {
 
         this.rowMapper.push("return function (row) { return {");
 
-        this.sql.from.addLine(this.schema.identifierAsSql(database.type, table.name) + " AS " + rootFromAlias);
+        this.sql.from.addLine(this.schema.getTable3PartName(table) + " AS " + rootFromAlias);
 
         for (let field of this.info.fieldNodes[0].selectionSet.selections) {
             if (field.kind == 'Field') {
@@ -182,57 +155,6 @@ export class SqlResolver {
             }
             else
                 throw "resolve_query(): todo: for field.kind:" + field.kind;
-            // if (field.kind == 'Field') {
-            //     let f = field as FieldNode;
-            //     // let f_alias = f.name.value;
-            //     // if (f.alias && f.alias.value)
-            //     //     f_alias = f.alias.value;
-            //     if (f.name.kind == "Name") {
-
-            //         let col = this.schema.getTableColumnByAlias(table, f.name.value);
-
-            //         if (col.ref_table) {
-            //             this.processFieldNode(f, database, table, col, rootFromAlias, "");
-            //         }
-            //         else {
-            //             let is_hidden = false;
-
-            //             for (let arg of f.arguments) {
-            //                 if (arg.kind != "Argument")
-            //                     throw "internal argument error";
-            //                 //if (arg.name.value.startsWith("where_")) {    
-
-            //                 if (this.schema.where_opers[arg.name.value]) {
-            //                     let where_oper = this.schema.where_opers[arg.name.value];
-
-            //                     let p0 = rootFromAlias + "." + this.schema.identifierAsSql(database.type, col.name);
-            //                     let p1: string;
-
-            //                     if (where_oper.sql_string.indexOf("%1") > -1) {
-            //                         p1 = this.argValueToSql(database.type, arg.value, arg.name.value.indexOf("between") > -1);
-            //                     }
-
-            //                     let whereStr = where_oper.sql_string.replace("%1", p1 || "").replace("%0", p0 || "");
-
-            //                     this.sql.where.add(whereStr);
-            //                 }
-            //                 else if (arg.name.value == "is_hidden") {
-            //                     is_hidden = true;
-            //                 }
-            //                 else
-            //                     throw new Error("todo: unknown where_eq param: " + arg.name.value);
-
-            //             }
-
-            //             if (!is_hidden)
-            //                 this.sql.fields.add(rootFromAlias + "." + this.schema.identifierAsSql(database.type, col.name) + " AS " + f.name.value);
-            //         }
-            //     }
-            //     else
-            //         throw new Error("todo: f.name.kind == Name");
-            // }
-            // else
-            //     throw new Error("todo: field.kind == 'Field'");
         }
         this.rowMapper.push("}}");
 
@@ -240,7 +162,7 @@ export class SqlResolver {
         console.log(this.sql.toSql());
         let execute_result = await this.schema.sqlExecute("бухта-wms", this.sql.toSql());
         //console.log(execute_result.recordset.map( this.rowMapper));
-        console.log(this.rowMapper.join("\n"));
+        //console.log(this.rowMapper.join("\n"));
 
         let mapperFunc = new Function(this.rowMapper.join("\n"))();
         return execute_result.recordset.map(mapperFunc);
